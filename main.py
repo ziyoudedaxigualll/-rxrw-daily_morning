1fcae07d6fdda6b57e09ca192baac19ec8311b5f
ziyoudedaxigualll <112110891+ziyoudedaxigualll@users.noreply.github.com>
2022-09-21 20:02:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,11 +29,6 @@ def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
